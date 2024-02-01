0b0c35db10b57fb65ef58c0a6ebcea8b7f086779
Ditebogo04 <116106777+Ditebogo04@users.noreply.github.com>
2024-02-01 08:04:40 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OnlineStoreApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
